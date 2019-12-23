a27097dbf54200c9323920aecab3e52276f55ce2
Vladimir Drannikov <krowen@inbox.ru>
2019-12-23 16:41:51 +0600

    Добавил адаптивные состояния для меню и каталога. Доработал открытие/закрытие пунктов меню

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,13 +1,48 @@
 const menu = document.querySelector('.list-menu');
+const menuItems = Array.from(menu.querySelectorAll('.list-menu__item span'));
 
-menu.addEventListener('click', function(evt) {
-  const menuItem = evt.target;
-  const innerMenu = menuItem.querySelector('.list-inner');
-  innerMenu.classList.toggle('hidden');
+menuItems.forEach(function(menuItem) {
+  menuItem.addEventListener('click', function(evt) {
+    const menuInner = menuItem.parentNode.querySelector('.list-inner');
 
-  document.addEventListener('click', function(evt) {
-    if (evt.target !== menuItem) {
-      innerMenu.classList.add('hidden');
+    if (menuInner.classList.contains('hidden')) {
+      const menuInnerAll = Array.from(menu.querySelectorAll('.list-inner'));
+
+      menuInnerAll.forEach(function(menu) {
+        menu.classList.add('hidden');
+      });
+      menuInner.classList.remove('hidden');
+
+      Array.from(menu.querySelectorAll('.list-menu__item')).forEach(function(item) {
+        item.style.backgroundColor = '';
+      });
+      menuItem.parentNode.style.backgroundColor = '#C22A2E';
+    } else {
+      menuInner.classList.add('hidden');
+      menuItem.parentNode.style.backgroundColor = '';
+    }
+  });
+
+  menuItem.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 13) {
+      const menuInner = menuItem.parentNode.querySelector('.list-inner');
+
+      if (menuInner.classList.contains('hidden')) {
+        const menuInnerAll = Array.from(menu.querySelectorAll('.list-inner'));
+
+        menuInnerAll.forEach(function(menu) {
+          menu.classList.add('hidden');
+        });
+        menuInner.classList.remove('hidden');
+
+        Array.from(menu.querySelectorAll('.list-menu__item')).forEach(function(item) {
+          item.style.backgroundColor = '';
+        });
+        menuItem.parentNode.style.backgroundColor = '#C22A2E';
+      } else {
+        menuInner.classList.add('hidden');
+        menuItem.parentNode.style.backgroundColor = '';
+      }
     }
   });
 });
